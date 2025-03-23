52643771e82b2bef599e51ebdac7e0c037b4cba6
Lukinhas <143443695+ThatLukinhasGuy@users.noreply.github.com>
2025-03-22 22:54:27 -0300

    Balls Hard Jr V

--- manager/run.sh
+++ manager/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build -t openshapes -f Dockerfile ..
+echo "Building OpenShapes Docker image..."
+docker build -t openshapes -f Dockerfile .. > /dev/null 2>&1
+echo "Starting the OpenShapes manager bot..."
 python3 -B -m manager
